bccf8d41ec1402a5811b486d0f528ac3d7eaf7aa
faizul <36614551+mdfaizulislam@users.noreply.github.com>
2024-04-21 05:58:29 +0600

    no need

--- src/employees/employee.module.ts
+++ src/employees/employee.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { EmployeeService } from './employee.service';
 import { EmployeeController } from './employee.controller';
-import { DatabaseModule } from 'src/database.providers';
 import { employeeProviders } from './employee.providers';
 
 @Module({
-  imports: [DatabaseModule],
   controllers: [EmployeeController],
   providers: [EmployeeService, ...employeeProviders],
   exports: [EmployeeService, ...employeeProviders]
