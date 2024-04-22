ed01798ecba1dbc42260b4700add949c9a5ff139
faizul <36614551+mdfaizulislam@users.noreply.github.com>
2024-04-22 21:03:17 +0600

    updated test spec

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,28 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { AuthModule } from './auth.module';
-import { JwtModule, JwtService } from '@nestjs/jwt';
-import { LocalStrategy } from './strategies/local.strategy';
-import { JwtStrategy } from './strategies/jwt.strategy';
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let authService: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [JwtModule.register({
-        secret: 'abcd123',
-        signOptions: { expiresIn: '1h' },
-      })],
-      providers: [AuthService, LocalStrategy, JwtStrategy],
+      providers: [{
+        provide: AuthService,
+        useValue: {
+          constructor: jest.fn(),
+          validateUser: jest.fn(),
+        },
+      }],
       controllers: [AuthController],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
+    authService = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should be defined', () => {
+    expect(authService).toBeDefined();
   });
 });
